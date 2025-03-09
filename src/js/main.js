3ba89109cf7aa5e8e1b3a73ef04fa7ac2635181c
sssBu4 <skleto2004denis@gmail.com>
2025-03-10 01:34:59 +0200

    comeback

--- src/js/main.js
+++ src/js/main.js
@@ -12,50 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
             clickable: true
         }
     });
-  
-    // Ініціалізація Swiper для обкладинок аніме
-    const swiperPop = new Swiper(".mySwiper-pop", {
-      slidesPerView: 5,  // Кількість видимих слайдів
-      spaceBetween: 25,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-  
-    var appendNumber = 4;
-    var prependNumber = 1;
-  
-    // Додавання слайдів
-    document.querySelector(".prepend-2-slides").addEventListener("click", function (e) {
-      e.preventDefault();
-      swiperPop.prependSlide([
-        '<div class="swiper-slide-pop">Slide ' + --prependNumber + "</div>",
-        '<div class="swiper-slide-pop">Slide ' + --prependNumber + "</div>",
-      ]);
-    });
-  
-    document.querySelector(".prepend-slide").addEventListener("click", function (e) {
-      e.preventDefault();
-      swiperPop.prependSlide(
-        '<div class="swiper-slide-pop">Slide ' + --prependNumber + "</div>"
-      );
-    });
-  
-    document.querySelector(".append-slide").addEventListener("click", function (e) {
-      e.preventDefault();
-      swiperPop.appendSlide(
-        '<div class="swiper-slide-pop">Slide ' + ++appendNumber + "</div>"
-      );
-    });
-  
-    document.querySelector(".append-2-slides").addEventListener("click", function (e) {
-      e.preventDefault();
-      swiperPop.appendSlide([
-        '<div class="swiper-slide-pop">Slide ' + ++appendNumber + "</div>",
-        '<div class="swiper-slide-pop">Slide ' + ++appendNumber + "</div>",
-      ]);
-    });
 
   // Панель пошуку
   const searchContainer = document.querySelector('.search-container');
